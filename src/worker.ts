1f8eac074da3762dd704cbb0894d4107222b6050
Sean Behan <codebam@riseup.net>
2023-08-16 20:53:18 -0400

    cleanup code

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,51 @@
-import handleProxy from './proxy';
+export interface Env {
+	telegram_secret: string;
+}
+
+const new_message = (token: string, text: string) =>
+	new URL(`https://api.telegram.org/bot${token}/sendMessage?` + new URLSearchParams({ chat_id: '@seanweblogschannel', text }));
 
 export default {
-	fetch: (request: Request, env: Env, ctx: ExecutionContext): Promise<Response> => handleProxy.fetch(request, env, ctx),
+	async fetch(request: Request, env: Env): Promise<Response> {
+		let proxy_url = '';
+		switch (new URL(request.url).host) {
+			case 'seanbehan.ca':
+				proxy_url = 'https://seanbehan-ca.pages.dev';
+			case 'www.seanbehan.ca':
+				proxy_url = 'https://seanbehan-ca.pages.dev';
+			default:
+				console.log(new URL(request.url).host);
+		}
+		let res = await fetch(proxy_url + new URL(request.url).pathname, request);
+
+		const ip = request.headers.get('x-real-ip');
+		const url = request.url;
+		const referer = request.headers.get('Referer');
+		const timezone = request.cf?.timezone;
+		const country = request.cf?.country;
+		const latitude = request.cf?.latitude;
+		const longitude = request.cf?.longitude;
+		const postal_code = request.cf?.postalCode;
+
+		await fetch(
+			new_message(
+				env.telegram_secret,
+				JSON.stringify(
+					{
+						ip,
+						url,
+						referer,
+						timezone,
+						country,
+						latitude,
+						longitude,
+						postal_code,
+					},
+					null,
+					2
+				)
+			)
+		);
+		return res;
+	},
 };
